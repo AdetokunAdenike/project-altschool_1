7a7432c6fba11e926adbddde19478920302fe358
Ubuntu <ubuntu@ip-172-31-47-225.eu-west-1.compute.internal>
2025-06-12 23:52:58 +0000

    Add version 1.0

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'static')));
+
+// Start the server
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Node server is running at http://localhost:${PORT}`);
+});
